980fec85fff69813db73399cf4d4d0964924ad0c
rangjin <46679581+rangjin@users.noreply.github.com>
2023-09-26 14:27:38 +0900

    init

---
+++ src/main/kotlin/com/better/betterbackend/BetterBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.better.betterbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BetterBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<BetterBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/better/betterbackend/BetterBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.better.betterbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BetterBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
