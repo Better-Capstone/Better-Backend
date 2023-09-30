80380c543f54701a2d1f3dcbfe0f15366bf8e4f8
rangjin <46679581+rangjin@users.noreply.github.com>
2023-09-30 19:57:55 +0900

    swagger 추가

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -10,5 +10,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
     runtimeOnly("com.mysql:mysql-connector-j")
 }
