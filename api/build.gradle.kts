630c85a4e894cc692d60e518093a1a6796df76bb
rangjin <46679581+rangjin@users.noreply.github.com>
2023-10-27 07:33:59 +0900

    test db 추가

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -3,4 +3,5 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
     runtimeOnly("com.mysql:mysql-connector-j")
+    testRuntimeOnly("com.h2database:h2:1.4.200")
 }
