20f2646ea795fea12bc35f9d7fae4a47604c4e68
rangjin <46679581+rangjin@users.noreply.github.com>
2023-11-07 15:59:11 +0900

    JSON 포맷 에러 추가

--- api/src/main/kotlin/com/better/betterbackend/global/exception/handler/ExceptionHandler.kt
+++ api/src/main/kotlin/com/better/betterbackend/global/exception/handler/ExceptionHandler.kt
@@ -1,11 +1,13 @@
 package com.better.betterbackend.global.exception.handler
 
 import com.better.betterbackend.global.exception.CustomException
+import com.better.betterbackend.global.exception.ErrorCode
 import com.better.betterbackend.global.exception.dto.ExceptionResponseDto
 import com.better.betterbackend.global.exception.dto.ValidationErrorFieldDto
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -44,6 +46,22 @@ class ExceptionHandler {
                 status = HttpStatus.BAD_REQUEST,
                 requestURI = request.requestURI,
                 data = data
+            )
+        )
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    protected fun jsonFormatError(
+        e: HttpMessageNotReadableException,
+        request: HttpServletRequest
+    ): ResponseEntity<ExceptionResponseDto<String>> {
+        val error = ErrorCode.JSON_FORMAT_ERROR
+
+        return ResponseEntity.status(error.status).body(
+            ExceptionResponseDto(
+                status = error.status,
+                requestURI = request.requestURI,
+                data = error.message
             )
         )
     }
