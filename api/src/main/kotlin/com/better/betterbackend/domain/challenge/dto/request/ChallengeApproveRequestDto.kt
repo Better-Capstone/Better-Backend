b293c9d65f727eab1128d994c377207148693213
rangjin <46679581+rangjin@users.noreply.github.com>
2023-11-22 12:25:21 +0900

    Challenge 모듈 리퀘스트 Validation 추가

--- api/src/main/kotlin/com/better/betterbackend/domain/challenge/dto/request/ChallengeApproveRequestDto.kt
+++ api/src/main/kotlin/com/better/betterbackend/domain/challenge/dto/request/ChallengeApproveRequestDto.kt
@@ -1,7 +1,11 @@
 package com.better.betterbackend.domain.challenge.dto.request
 
+import com.better.betterbackend.global.validation.ValidationGroup
+import jakarta.validation.constraints.NotNull
+
 data class ChallengeApproveRequestDto (
 
+    @field:NotNull(message = "승인 여부는 필수 값 입니다", groups = [ValidationGroup.NotNullGroup::class])
     val approved : Boolean
 
 ){
--- api/src/main/kotlin/com/better/betterbackend/domain/challenge/dto/request/ChallengeRegisterRequestDto.kt
+++ api/src/main/kotlin/com/better/betterbackend/domain/challenge/dto/request/ChallengeRegisterRequestDto.kt
@@ -1,11 +1,19 @@
 package com.better.betterbackend.domain.challenge.dto.request
 
+import com.better.betterbackend.global.validation.ValidationGroup
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
 data class ChallengeRegisterRequestDto (
 
+    @field:NotBlank(message = "챌린지 제목은 필수 값 입니다", groups = [ValidationGroup.NotNullGroup::class])
+    @field:Size(min = 1, max = 20, message = "최소 1글자, 최대 20글자의 문자열 이어야 합니다", groups = [ValidationGroup.SizeGroup::class])
     val title : String,
 
+    @field:NotBlank(message = "챌린지 이미지는 필수 값 입니다", groups = [ValidationGroup.NotNullGroup::class])
     val image : String,
 
+    @field:NotBlank(message = "챌린지 설명은 필수 값 입니다", groups = [ValidationGroup.NotNullGroup::class])
     val description : String,
 
 ){
