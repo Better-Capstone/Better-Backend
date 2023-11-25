485560c9a4f8fad9e42bfab1648608d5b244156e
rangjin <46679581+rangjin@users.noreply.github.com>
2023-11-25 22:17:05 +0900

    test 용 api 삭제 & task id로 조회 api 추가

--- api/src/main/kotlin/com/better/betterbackend/domain/user/web/UserController.kt
+++ api/src/main/kotlin/com/better/betterbackend/domain/user/web/UserController.kt
@@ -3,8 +3,6 @@ package com.better.betterbackend.domain.user.web
 
 import com.better.betterbackend.domain.challenge.dto.ChallengeDto
 import com.better.betterbackend.domain.task.dto.TaskDto
-
-
 import com.better.betterbackend.domain.user.service.UserService
 import com.better.betterbackend.domain.user.dto.request.UserRegisterRequestDto
 import com.better.betterbackend.domain.user.dto.response.UserCheckResponseDto
@@ -26,12 +24,6 @@ class UserController(
     private val userService: UserService,
 
 ) {
-
-    @Operation(summary = "유저 생성(테스트 용)")
-    @GetMapping("/test")
-    fun test(): ResponseEntity<List<String>> {
-        return ResponseEntity.ok().body(userService.test())
-    }
 
     @Operation(summary = "유저 토큰 생성(테스트 용)")
     @GetMapping("/token/{id}")
